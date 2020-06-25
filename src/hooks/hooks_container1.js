0053e05036a21ee6344065ea79fc71747d1606c4
fvilca1 <fvilca@gmail.com>
2020-06-25 04:39:27 -0500

    handle useReducer to change a state value by dispatch methods(actions)

--- src/hooks/hooks_container1.js
+++ src/hooks/hooks_container1.js
@@ -1,27 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
+import * as Reducer_ from '../store/hooks_state/hooks_reducer'
+import * as Actions from '../store/actions/actions'
 
 function HooksContainer1() {
 
-  const [stateValue, setValue] = React.useState(0)
-  const [useEffectValue, setUseEffectValue] = React.useState(null)
 
+  //+++++++++++++ state
+  const [stateValue, setValue] = useState(0)
+  const [useEffectValue, setUseEffectValue] = useState(null)
+  const [state, dispatch] = useReducer(Reducer_.HooksReducer, Reducer_.initialState)
+  const testHook = (useEffectValue) ? useEffectValue : 'espera 3 segundos'
+
+  //+++++++++++++ methods
   const incrementValue = (factor) => {
     setValue(stateValue + factor)
   }
-
-  React.useEffect(() => {
-    setTimeout(
-      ()=>setUseEffectValue("El componente se ha montado"),3000
+  useEffect(() => {
+    setTimeout(() => setUseEffectValue("El componente se ha montado"), 3000
     )
-    
+
   }, [stateValue])
-
-  const testHook = (useEffectValue)? useEffectValue: 'espera 3 segundos'
+  const changeUseEffectValue = () => {
+    setUseEffectValue("some string")
+  }
   
- const changeUseEffectValue=()=>{
-  setUseEffectValue("some string")
- }
-
+  const handleDispatchTrue = () => {
+    dispatch(Actions.success())
+  }
+  const handleDispatchFalse = () => {
+    dispatch(Actions.failure())
+  }
 
   return (
     <div>
@@ -29,7 +37,12 @@ function HooksContainer1() {
       <button onClick={() => incrementValue(1)}>Increment</button>
       <button onClick={() => incrementValue(-1)}>Decrement</button>
       <button onClick={() => changeUseEffectValue()}>change</button>
-      
+      <button onClick={() => handleDispatchTrue()}>change By Reducer True</button>
+      <button onClick={() => handleDispatchFalse()}>change By Reducer False</button>
+      {state.steteprop1
+        ? <p>the state prop is true</p>
+        : <p>the state prop is false</p>
+      }
       <p>{testHook} </p>
     </div>
   )
