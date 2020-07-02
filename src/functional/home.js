d45e222d5dd899a318403509bd54154d8f4cc994
fvilca1 <fvilca@gmail.com>
2020-07-01 19:42:21 -0500

    consume webservices person data with Axios

--- src/functional/home.js
+++ src/functional/home.js
@@ -1,5 +1,6 @@
-import React,{useContext} from 'react'
+import React,{useContext, useEffect, useState} from 'react'
 import Context from '../utils/context';
+import axios from 'axios'
 
 //Home page
 const Home = (props) => {
@@ -7,11 +8,32 @@ const Home = (props) => {
   console.log("Home: props",props)
   const context = useContext(Context)
 
+  const [data, setData] = useState([])
+
+  const url = 'https://jsonplaceholder.typicode.com/users'
+  useEffect(() => {
+    axios.get(url)
+    .then(res=>{const persons = res.data;
+      setData(persons)
+    })
+    return () => {
+      
+    }
+  }, [])
   return(
     <div>
       Home:
       <br/>
       globalValue:{context.globalValue}
+
+      <section>
+        {data.map(
+          (person)=>
+            <ul>
+              <li>{person.name}:{person.phone}& {person.website} </li>
+            </ul>
+        )}
+      </section>
     </div>
   )
 };
